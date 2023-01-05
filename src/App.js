01078cd78c38559ea4add314505f0ee1c7c38ce2
ruhollah_jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2023-01-05 19:27:26 +0330

    Use react-router-dom and switch between pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
+import { Routes, Route } from "react-router-dom"
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Hello
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
