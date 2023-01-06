b1cfe9bef641e801598befc717baa3e9dc21b09a
ruhollah_jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2023-01-06 14:01:59 +0330

    Test login

--- src/App.js
+++ src/App.js
@@ -25,9 +25,9 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
+        <Routes> 
+          <Route path="/" element={<Home />} /> 
+          <Route path="about" element={<About />} /> 
           <Route path="contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>  
