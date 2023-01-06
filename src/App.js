f9422644391af92d65a5eb3c95385723ed17c1e6
ruhollah_jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2023-01-06 10:07:58 +0330

    Add token and Autorize

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+import React,{useState} from "react";
 import { Routes, Route } from "react-router-dom"
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home'
 import About from './Components/About'
 import Contact from './Components/Contact'
+import Login from './Components/Login/Login'
+import useToken from './useToken';
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
 
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
 function App() {
+  const { token, setToken } = useToken();
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,7 @@ function Home() {
         <h1>This is the home page</h1>
         <Link to="about">Click to view our about page</Link>
         <Link to="contact">Click to view our contact page</Link>
+        <Link to="login">Login</Link>
       </div>
       );
     }
