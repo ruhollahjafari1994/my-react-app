a56a189210e661e0433f03eb112ff667e0f321eb
ruhollah_jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2023-01-06 13:57:26 +0330

    Add token and Autorize

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter>  
     </div>
   );
 }
